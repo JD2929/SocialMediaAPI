3413e43808b8ca827450dd55e8ab3b8edde2a971
JD2929 <jandunk@creonova.com>
2023-10-04 21:29:06 -0300

    Thought and reaction models created

--- models/reactionModel.js
+++ models/reactionModel.js
@@ -6,6 +6,19 @@ const reactionSchema = new Schema(
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId(),
         },
- reactionBody
+ reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+ },
+ username: {
+    type: String,
+    required: true,
+
+},
+createdAt: {
+    type: Date,
+    default: Date.now,
+},
     }
 )
