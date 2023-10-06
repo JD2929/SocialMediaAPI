6d9c76a735151378b17b2d248a5c1fddb2a5be5a
JD2929 <jandunk@creonova.com>
2023-10-06 17:00:51 -0300

    User routes coded but not yet tested.

---
+++ controllers/userControllers.js
@@ -0,0 +1,33 @@
+const { User, Thought } = require('../models');
+
+const userQueries = {
+    getUsers(req, res) {
+        User.find()
+            .select('-__v')
+            .then((users) => {
+                res.json(users)
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
+    },
+
+    getOneUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then((user) => {
+                !user ? res.status(404).json({message:'No user found with this ID'}): res.json(users)
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
+    },
+
+    createUser(req, res) {
+        User.create(req.body)
+        .then((user) => res.json(user))
+        .catch ((err) =>  res.status(500).json(err))
+    }
+}
+
+module.exports = userQueries;
