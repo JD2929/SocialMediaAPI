76df7a9c8f36d00aa06efea05bfea766d8df4470
JD2929 <jandunk@creonova.com>
2023-10-06 16:37:02 -0300

    Finished user Model

--- models/Thought.js
+++ models/Thought.js
@@ -36,4 +36,6 @@ thoughtSchema.virtual('reactionCount').get (function(){
     return this.reactions.length
 })
 
-const Thought = model('thought')
+const Thought = model('thought', thoughtSchema)
+
+module.exports = Thought;
